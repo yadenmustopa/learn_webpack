a23e4d9aabe5312414c79e492d24081e2fb9fc27
yaden <brildent@gmail.com>
2022-04-16 14:27:34 +0700

    test new config start

---
+++ webpack.config.js
@@ -0,0 +1,13 @@
+const path= require("path");
+
+const config = {
+    entry : "./src/app.js",
+    output : { 
+        path : path.resolve(__dirname,"dist"),
+        filename : "bundle.js",
+    }
+
+}
+
+
+module.exports = config;
